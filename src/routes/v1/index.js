fb4deba18c0de1b57f29c97352c217a123ffecdb
Jun <jeffery851007@gmail.com>
2025-05-05 22:17:23 +0800

    fix: activities routes (#7)

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const authRouter = require('./auth');
 const userRouter = require('./user');
+const activityRouter = require('./activities');
 
 const router = express.Router();
 
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
+router.use('/activities', activityRouter);
 
 module.exports = router;
